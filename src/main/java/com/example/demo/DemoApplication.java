4280b6df3eab3990af53a234cfa547bd425e47db
Ajay Paul <a4ajay619@gmail.com>
2022-02-20 13:01:39 +0530

    SpringBootServiceIntializer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,14 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+			return application.sources(DemoApplication.class);
+	}
 
-	public static void main(String[] args) {
-
-		System.out.println("Hello world");
-
+	public static void main(String[] args) throws Exception {
+		System.out.println("hello deployed");
 	}
 
 }
